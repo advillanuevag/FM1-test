681c3e3931618b094c73df7b0a288817d80dd56e
Alberto Villanueva <50636552+advillanuevag@users.noreply.github.com>
2022-08-03 11:37:48 -0400

    Homework 01-IntroToCS

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,11 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var data = num.split("");
+  data = data.reverse();
+  var resultado = 0;
+  for (let i = 0; i < data.length; i++) {
+    var factor = 2 ** i;
+    resultado = resultado + factor * data[i];
+  }
+
+  return resultado;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var result = "";
+  while (num > 0) {
+    result = (num % 2) + result;
+    num = Math.floor(num / 2);
+  }
+  //console.log(result);
+  return result;
 
 }
 
