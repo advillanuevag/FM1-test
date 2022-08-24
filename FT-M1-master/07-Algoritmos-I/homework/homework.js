f34e5f736870595d1512581471f65414aea13d6e
Alberto Villanueva <50636552+advillanuevag@users.noreply.github.com>
2022-08-23 23:24:45 -0400

    Homework 07-Algoritmos-I

--- FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,19 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let result = [1];
+  let i = 2;
+  while (num > 1) {
+    if (num % i === 0) {
+      result.push(i);
+      num = num / i;
 
+    } else {
+      i++;
+    }
+
+  }
+  return result;
 }
 
 function bubbleSort(array) {
@@ -14,7 +26,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let mover = true;
+  while (mover) {
+    mover = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let temporal = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temporal;
+        mover = true;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +47,17 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  //[5, 1, 4, 2, 8]
+  for (let i = 1; i < array.length; i++) {
+    var j = i - 1;
+    var aux = array[i];
+    while (j >= 0 && array[j] > aux) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = aux;
+  }
+  return array;
 }
 
 
@@ -32,7 +66,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let minimo = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minimo]) {
+        minimo = j;
+      }
+    }
+    let aux = array[i];
+    array[i] = array[minimo];
+    array[minimo] = aux;
+  }
+  return array;
 }
 
 
